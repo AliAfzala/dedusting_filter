dbc885c8de51519376bc4308b6b9a2ef3fcdb71f
Ali <aafsalaghaein@gmail.com>
2022-02-17 17:25:56 -0500

    Fixing the problem of matrix: It was resized in each loop while it is now resized for once. Recordong the time of executation.

--- voxelization/voxel/octree_search.cpp
+++ voxelization/voxel/octree_search.cpp
@@ -11,6 +11,8 @@
 #include<cmath>
 #include<iterator>
 #include<algorithm>
+#include<chrono>
+using namespace std::chrono;
 
 using namespace Eigen;
 
@@ -22,6 +24,7 @@ int main (int argc, char** argv)
   pcl::PointCloud<pcl::PointXYZI>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZI>);
   pcl::io::loadPCDFile <pcl::PointXYZI> ("dust-5m-4m-Frame77.pcd", *cloud1); 
   
+  auto start = high_resolution_clock::now();
 
   double minX = -10; double minY = 0; double minZ = -3;
   double maxX = 20 ; double maxY = 20 ; double maxZ = 3 ;
@@ -33,20 +36,8 @@ int main (int argc, char** argv)
     // For conversion of pcl::pointcloud2 to pcl::pointXYZI refer to following website: 
     //https://answers.ros.org/question/136916/conversion-from-sensor_msgspointcloud2-to-pclpointcloudt
 
-    /*pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    cloud->width = 1000;
-    cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
 
-    for (std::size_t i = 0; i < cloud->size (); ++i)
-  {
-    (*cloud)[i].x = 1024.0f * rand () / (RAND_MAX + 1.0f);
-    (*cloud)[i].y = 1024.0f * rand () / (RAND_MAX + 1.0f);
-    (*cloud)[i].z = 1024.0f * rand () / (RAND_MAX + 1.0f);
-  }
-  */
-
-  float resolution = 0.25f;
+  float resolution = 0.20f;
   //pcl::octree::OctreePointCloudPointVector<pcl::PointXYZ> octree (resolution);
   pcl::octree::OctreePointCloudSearch<pcl::PointXYZI> octree (resolution);
   //octree.defineBoundingBox (-10.0 , 0.0 ,  -3.0 ,  20.0 ,  20.0 ,  3.0);
@@ -76,7 +67,7 @@ int main (int argc, char** argv)
  
  std::vector<int> pointIdxVec;
  std::vector<int> voxel_intensity;
- //int sz = searchPoint.size();
+
   int ii = 1;
   //for (std::size_t j = 0; j < searchPoint.size (); ++j)
   for (int j = 0; j < 100; ++j)
@@ -84,8 +75,10 @@ int main (int argc, char** argv)
     if ( (octree.voxelSearch (searchPoint[j], pointIdxVec)) && (pointIdxVec.size() > 3))
       {
         
-        int sz = searchPoint.size();
+        int sz = pointIdxVec.size();
+        //std::cout<< " number of points inside voxel is " << sz << std::endl ; 
         Matrix<double,Dynamic,3> voxel_points;
+        voxel_points.conservativeResize(sz,3);
         //voxel_points.resize(sz,3);  
         //VectorXd voxel_intensity ; 
         //Matrix<float,Dynamic,1> voxel_intensity;
@@ -94,7 +87,7 @@ int main (int argc, char** argv)
         
         for (std::size_t i = 0; i < pointIdxVec.size (); ++i)
             {
-            voxel_points.conservativeResize(i+1,3);
+            //voxel_points.conservativeResize(i+1,3);
             //voxel_intensity.conservativeResize(i+1,1); 
             //std::cout<<i<< std::endl;
             voxel_points (i,0) = (*cloud)[pointIdxVec[i]].x ; 
@@ -106,11 +99,6 @@ int main (int argc, char** argv)
             voxel_intensity.push_back((*cloud)[pointIdxVec[i]].intensity);
            
             } 
-            /*
-            std::cout << "start" << std::endl;
-            std::cout << voxel_points << std::endl;
-            std::cout << "end" << std::endl;
-            */
 
 
         
@@ -120,9 +108,9 @@ int main (int argc, char** argv)
 
         double voxel_roughness = std::abs(D(2,2)); 
         double voxel_slope = std::sin( V(2,2)/ V.col(2).norm() );
-        std::cout << " Voxel number " << ii << std::endl; 
-        std::cout << " Voxel roughness is:  " << voxel_roughness << std::endl ;  
-        std::cout << " Voxel slope is:  "<< voxel_slope << std::endl ;
+        //std::cout << " Voxel number " << ii << std::endl; 
+        //std::cout << " Voxel roughness is:  " << voxel_roughness << std::endl ;  
+        //std::cout << " Voxel slope is:  "<< voxel_slope << std::endl ;
         //std::cout << " EigenValueMatrix starts"<< std::endl;
         //std::cout << D << std::endl;
         //std::cout << V << std::endl;
@@ -139,19 +127,19 @@ int main (int argc, char** argv)
           double voxel_intensity_mean = sum / voxel_intensity.size();
           double sq_sum = std::inner_product(voxel_intensity.begin(), voxel_intensity.end(), voxel_intensity.begin(), 0.0);
           double voxel_intensity_std = std::sqrt(sq_sum / voxel_intensity.size() - voxel_intensity_mean *voxel_intensity_mean);
-          std::cout << " Voxel mean intensity is:  " << voxel_intensity_mean << std::endl ;
-          std::cout << " Voxel std intensity is:  " << voxel_intensity_std << std::endl ;
+          //std::cout << " Voxel mean intensity is:  " << voxel_intensity_mean << std::endl ;
+          //std::cout << " Voxel std intensity is:  " << voxel_intensity_std << std::endl ;
           pointIdxVec.clear();
         }
       ii++;
       
       
   }
-  //std::cout<< " the size of voxel center vector is  " << searchPoint.size()<< std::endl;
-  //std::cout<< " the size of voxel having point more than 3 " <<  ii << std::endl;
-  //std::cout<< " the size of voxel having point more than 3 " << octree.getResolution() << std::endl;
-    // Success
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  std::cout << "Time taken for one loop: "
+         << duration.count() << " microseconds" << std::endl;
+
     return 0;
   }
 
- // Eigen::Matrix3f covariance_matrix;
